17af7458439f6410fe6318562340a5d05fabb340
Pradel Léo <pradel.leo@gmail.com>
2017-07-28 17:03:35 +0200

    Fix theme light

--- src/constants.ts
+++ src/constants.ts
@@ -92,24 +92,7 @@ export const introspectionQuery = `
   }
 `
 
-export const defaultQuery = `# Welcome to Graphcool's custom GraphiQL ✌
-#
-# GraphiQL is an in-browser IDE for writing, validating, and
-# testing GraphQL queries.
-#
-# In Graphcool's custom GraphiQL you can additionally generate
-# code examples and download the result JSON.
-#
-# Type queries into this side of the screen, and you will
-# see intelligent typeaheads aware of the current GraphQL type schema and
-# live syntax and validation errors highlighted within the text.
-#
-# To bring up the auto-complete at any point, just press Ctrl-Space.
-#
-# Press the run button above, or Cmd-Enter to execute the query, and the result
-# will appear in the pane to the right.
-
-`
+export const defaultQuery = '# Try to write your query here'
 
 export const modalStyle = {
   overlay: {
