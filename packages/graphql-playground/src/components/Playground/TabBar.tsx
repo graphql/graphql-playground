9ccbe6f19f371a5accd4461fe86720cb953654a2
timsuchanek <tim.suchanek@gmail.com>
2017-10-14 13:36:26 +0200

    fix react key error

--- packages/graphql-playground/src/components/Playground/TabBar.tsx
+++ packages/graphql-playground/src/components/Playground/TabBar.tsx
@@ -180,6 +180,7 @@ export const TabBar = withTheme<
         <div className={cx('tabs', { isApp })}>
           {sessions.map((session, index) =>
             <Tab
+              key={session.id}
               session={session}
               index={index}
               onSelectSession={onSelectSession}
