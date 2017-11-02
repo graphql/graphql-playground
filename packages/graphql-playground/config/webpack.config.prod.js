b3e8496ef96414845d336033842b4dafbb37b968
timsuchanek <tim.suchanek@gmail.com>
2017-11-02 11:24:24 +0100

    make svg inline

--- packages/graphql-playground/config/webpack.config.prod.js
+++ packages/graphql-playground/config/webpack.config.prod.js
@@ -218,6 +218,10 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.svg$/,
+            loader: require.resolve('svg-inline-loader')
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader don't uses a "test" so it will catch all modules
