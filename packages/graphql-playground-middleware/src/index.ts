82b22b88eb9a3fce8514ffa27b629e260381ec24
timsuchanek <tim.suchanek@gmail.com>
2017-11-02 16:35:18 +0100

    add serverless middleware

--- packages/graphql-playground-middleware/src/index.ts
+++ packages/graphql-playground-middleware/src/index.ts
@@ -2,10 +2,12 @@ import { default as expressPlayground } from './express'
 import { default as hapiPlayground } from './hapi'
 import { default as koaPlayground } from './koa'
 import { default as renderPlaygroundPage } from './render-playground-page'
+import { default as lambdaPlayground } from './lambda'
 
 export {
   expressPlayground,
   hapiPlayground,
   koaPlayground,
   renderPlaygroundPage,
+  lambdaPlayground,
 }
