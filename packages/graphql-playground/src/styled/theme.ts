431a72da9fea9008a70c58145165d86ce9f45616
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-11-20 12:26:23 +0330

    refactor(to-sc): Share -- w/o Button

--- packages/graphql-playground/src/styled/theme.ts
+++ packages/graphql-playground/src/styled/theme.ts
@@ -1,5 +1,9 @@
 export interface Colours {
   green: string
+  darkBlue: string
+  darkBlue50: string
+  darkBlue60: string
+  darkBlue10: string
   black40: string
   paleText: string
   paleGrey: string
@@ -7,6 +11,10 @@ export interface Colours {
 
 export const colours: Colours = {
   green: '#27ae60',
+  darkBlue: 'rgb(23, 42, 58)',
+  darkBlue50: 'rgba(23, 42, 58, 0.5)',
+  darkBlue60: 'rgba(23, 42, 58, 0.6)',
+  darkBlue10: 'rgba(23, 42, 58, 0.1)',
   black40: 'rgba(0, 0, 0, 0.4)',
 
   paleText: 'rgba(0, 0, 0, 0.5)',
@@ -15,16 +23,29 @@ export const colours: Colours = {
 
 export interface Sizes {
   small6: string
+  small10: string
   small12: string
   small16: string
   smallRadius: string
+  fontSemiBold: string
+  fontTiny: string
+  fontSmall: string
 }
 
 export const sizes: Sizes = {
   small6: '6px',
+  small10: '10px',
   small12: '12px',
   small16: '16px',
 
+  // font weights
+  fontSemiBold: '600',
+
+  // font sizes
+  fontTiny: '12px',
+  fontSmall: '14px',
+
+  // others
   smallRadius: '2px',
 }
 
