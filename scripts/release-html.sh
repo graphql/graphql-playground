0abb61c0d350559141cccd1d11d7531ce6aa966d
Johannes Schickling <schickling.j@gmail.com>
2018-01-03 20:40:12 +0100

    chore: released new versions react & html

--- scripts/release-html.sh
+++ scripts/release-html.sh
@@ -15,13 +15,14 @@ cd graphql-playground-html
 echo "Releasing graphql-playground-html..."
 npm version patch --no-git-tag-version
 npm publish
+version=$(cat package.json | jq -r '.version')
 cd ..
 
 for middleware in "${middlewares[@]}"
 do
   cd $middleware
   echo "Releasing ${middleware}..."
-  yarn add graphql-playground-html
+  yarn add graphql-playground-html@$version
   npm version patch --no-git-tag-version
   npm publish
   cd ..
--- scripts/release-react.sh
+++ scripts/release-react.sh
@@ -11,18 +11,37 @@ middlewares=(
   # graphql-playground-middleware-lambda
 )
 
-cd graphql-playground-html
+cd graphql-playground-react
 echo "Releasing graphql-playground-react..."
 npm version patch --no-git-tag-version
 npm publish
+version=$(cat package.json | jq -r '.version')
 cd ..
 
 for middleware in "${middlewares[@]}"
 do
   cd $middleware
   echo "Releasing ${middleware}..."
-  yarn add graphql-playground-react
+  yarn add graphql-playground-react@$version
   npm version patch --no-git-tag-version
   npm publish
   cd ..
 done
+
+cd graphql-playground-electron
+echo "Updating dependency & version in graphql-playground-electron..."
+yarn add graphql-playground-react@$version
+npm version patch --no-git-tag-version
+cd ..
+
+echo "Updating JSDeliver cache..."
+curl -X POST \
+  http://purge.jsdelivr.net/ \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -d '{
+	"path": [
+		"/npm/graphql-playground-react/middleware-build/static/css/main.css",
+		"/npm/graphql-playground-react/middleware-build/static/js/main.js"
+	]
+}'
