2ee16d1f6129bd618d5ad49b0fb7475987c71915
Johannes Schickling <schickling.j@gmail.com>
2018-01-03 20:28:06 +0100

    added scripts

---
+++ scripts/release-html.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+cd packages
+
+middlewares=(
+  graphql-playground-middleware-express
+  # graphql-playground-middleware-hapi
+  # graphql-playground-middleware-koa
+  # graphql-playground-middleware-lambda
+)
+
+cd graphql-playground-html
+echo "Releasing graphql-playground-html..."
+npm version patch --no-git-tag-version
+npm publish
+cd ..
+
+for middleware in "${middlewares[@]}"
+do
+  cd $middleware
+  echo "Releasing ${middleware}..."
+  yarn add graphql-playground-html
+  npm version patch --no-git-tag-version
+  npm publish
+  cd ..
+done
