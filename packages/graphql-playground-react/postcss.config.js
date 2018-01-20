ec6084bf16b0552fbf880bcb2fed20131fb4b715
timsuchanek <tim.suchanek@gmail.com>
2018-01-20 11:25:12 +0100

    refactor(yarn): reenable workspaces

--- packages/graphql-playground-react/postcss.config.js
+++ packages/graphql-playground-react/postcss.config.js
@@ -5,7 +5,7 @@ module.exports = (ctx) => ({
     }),
     require('postcss-cssnext')(),
     require('postcss-inherit')({
-      globalStyles: 'node_modules/graphcool-styles/dist/styles.css',
+      globalStyles: '../../node_modules/graphcool-styles/dist/styles.css',
       propertyRegExp: /^(inherit|extend|p)s?:?$/i,
     }),
   ]
--- packages/graphql-playground-react/src/middlewareIndex.tsx
+++ packages/graphql-playground-react/src/middlewareIndex.tsx
@@ -4,7 +4,6 @@ import MiddlewareApp from './components/MiddlewareApp'
 import './styles/graphiql_dark.css'
 import './styles/graphiql_light.css'
 import './index.css'
-import 'graphcool-styles/dist/styles.css'
 
 if (process.env.NODE_ENV !== 'production') {
   /* tslint:disable-next-line */
