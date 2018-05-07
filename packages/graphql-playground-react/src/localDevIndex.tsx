526c475b20ad900e2d538e880064338d184c443d
Huvik <huvikjee@gmail.com>
2018-05-07 10:27:07 +0200

    Finished dark

--- packages/graphql-playground-react/src/localDevIndex.tsx
+++ packages/graphql-playground-react/src/localDevIndex.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import MiddlewareApp from './components/MiddlewareApp'
-import './styles/graphiql_dark.css'
-import './styles/graphiql_light.css'
 import './index.css'
 
 if (process.env.NODE_ENV !== 'production') {
