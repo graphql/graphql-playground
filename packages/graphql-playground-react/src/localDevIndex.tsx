a32649bc080ffd9e60988417c2fa6d60f7b2c1bc
Huvik <huvikjee@gmail.com>
2018-05-30 11:30:07 +0200

    Remove endpoint

--- packages/graphql-playground-react/src/localDevIndex.tsx
+++ packages/graphql-playground-react/src/localDevIndex.tsx
@@ -57,7 +57,7 @@ const config = {
         },
         endpoints: {
           dev2: {
-            url: 'https://justcluster_justmighty.prisma.sh/zonic/prod',
+            url: 'https://eu1.prisma.sh/public-asdf/session65/dev',
             // headers: {
             //   Authorization:
             //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJhc2RmQGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1MjM1MTg3NTYsImV4cCI6MTUyNDEyMzU1Nn0.fzKhXa1JpN9M1UGTbS6p2KMUWDrKLxYD3i3a9eVfOQQ',
