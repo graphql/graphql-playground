6dc97cbfa1fa16e4e7d13a87529c59171e171df7
Divyendu Singh <divyendu.z@gmail.com>
2018-06-28 19:17:59 +0200

    recover localDevIndex from master

--- packages/graphql-playground-react/src/localDevIndex.tsx
+++ packages/graphql-playground-react/src/localDevIndex.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import MiddlewareApp from './components/MiddlewareApp'
 import './index.css'
-import { Tab } from './state/sessions/reducers'
+// import { Tab } from './state/sessions/reducers'
 // import { LinkCreatorProps } from './state/sessions/fetchingSagas'
 // import { ApolloLink } from 'apollo-link'
 // import { HttpLink } from 'apollo-link-http'
@@ -14,15 +14,6 @@ if (process.env.NODE_ENV !== 'production') {
   // whyDidYouUpdate(React)
 }
 
-const tabs: Tab[] = [
-  {
-    name: 'Tab 1',
-    query: '{ users { id } }',
-    endpoint: 'https://eu1.prisma.sh/public-asdf/session65/dev',
-    responses: ['{}'],
-  },
-]
-
 /* tslint:disable-next-line */
 ;(window as any)['GraphQLPlayground'] = {
   init(element: HTMLElement, options) {
@@ -31,10 +22,10 @@ const tabs: Tab[] = [
         setTitle={true}
         showNewWorkspace={false}
         {...options}
-        // config={config}
-        // configString={configString}
+        config={config}
+        configString={configString}
         // codeTheme={lightEditorColours}
-        tabs={tabs}
+        // tabs={tabs}
         // createApolloLink={customLinkCreator}
         // schema={exampleSchema}
       />,
@@ -43,50 +34,62 @@ const tabs: Tab[] = [
   },
 }
 
-// const configString = `projects:
-// app:
-//   schemaPath: "src/schema.graphql"
-//   extensions:
-//     endpoints:
-//       default: "http://localhost:4000"
-// database:
-//   schemaPath: "src/generated/prisma.graphql"
-//   extensions:
-//     prisma: database/prisma.yml`
+const configString = `projects:
+app:
+  schemaPath: "src/schema.graphql"
+  extensions:
+    endpoints:
+      default: "http://localhost:4000"
+database:
+  schemaPath: "src/generated/prisma.graphql"
+  extensions:
+    prisma: database/prisma.yml`
 
-// const config = {
-//   projects: {
-//     prisma: {
-//       schemaPath: 'src/generated/prisma.graphql',
-//       includes: ['database/seed.graphql'],
-//       extensions: {
-//         prisma: 'database/prisma.yml',
-//         'prepare-binding': {
-//           output: 'src/generated/prisma.ts',
-//           generator: 'prisma-ts',
-//         },
-//         endpoints: {
-//           dev2: {
-//             url: 'https://eu1.prisma.sh/public-asdf/session65/dev',
-//             // headers: {
-//             //   Authorization:
-//             //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJhc2RmQGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1MjM1MTg3NTYsImV4cCI6MTUyNDEyMzU1Nn0.fzKhXa1JpN9M1UGTbS6p2KMUWDrKLxYD3i3a9eVfOQQ',
-//             // },
-//           },
-//         },
-//       },
-//     },
-//     app: {
-//       schemaPath: 'src/schema.graphql',
-//       includes: ['queries/{booking,queries}.graphql'],
-//       extensions: {
-//         endpoints: {
-//           default: 'http://localhost:4000',
-//         },
-//       },
-//     },
+const config = {
+  projects: {
+    prisma: {
+      schemaPath: 'src/generated/prisma.graphql',
+      includes: ['database/seed.graphql'],
+      extensions: {
+        prisma: 'database/prisma.yml',
+        'prepare-binding': {
+          output: 'src/generated/prisma.ts',
+          generator: 'prisma-ts',
+        },
+        endpoints: {
+          dev2: {
+            url: 'https://eu1.prisma.sh/public-asdf/session65/dev',
+            // headers: {
+            //   Authorization:
+            //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJhc2RmQGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1MjM1MTg3NTYsImV4cCI6MTUyNDEyMzU1Nn0.fzKhXa1JpN9M1UGTbS6p2KMUWDrKLxYD3i3a9eVfOQQ',
+            // },
+          },
+        },
+      },
+    },
+    app: {
+      schemaPath: 'src/schema.graphql',
+      includes: ['queries/{booking,queries}.graphql'],
+      extensions: {
+        endpoints: {
+          default: 'http://localhost:4000',
+        },
+      },
+    },
+  },
+}
+
+// const tabs: Tab[] = [
+//   {
+//     query: '{ users { id } }',
+//     endpoint: 'https://eu1.prisma.sh/public-asdf/session65/dev',
+//     responses: ['{}'],
 //   },
-// }
+//   {
+//     query: '{ users { id } }',
+//     endpoint: 'https://eu1.prisma.sh/public-asdf/session65/dev',
+//   },
+// ]
 
 // const customLinkCreator = (
 //   session: LinkCreatorProps,
