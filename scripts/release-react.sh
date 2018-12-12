648dddffc6f3d08a6a2dd040b558c196be406206
Huvik <huvikjee@gmail.com>
2018-12-12 11:03:14 +0100

    New version and fix of purge cache

--- scripts/release-react.sh
+++ scripts/release-react.sh
@@ -31,8 +31,8 @@ curl -X POST \
   -H 'content-type: application/json' \
   -d '{
 	"path": [
-		"/npm/graphql-playground-react/middleware-build/static/css/main.css",
-		"/npm/graphql-playground-react/middleware-build/static/js/main.js"
+		"/npm/graphql-playground-react/build/static/css/middleware.css",
+		"/npm/graphql-playground-react/build/static/js/middleware.js"
 	]
 }'
 
