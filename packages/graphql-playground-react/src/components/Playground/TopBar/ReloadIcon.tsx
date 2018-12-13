75cd23eac44abe0cf62b812b88488a59e6be374b
Rohit <rohit@novvuminc.com>
2018-12-12 16:00:24 -0800

    moved icons to the left

--- packages/graphql-playground-react/src/components/Playground/TopBar/ReloadIcon.tsx
+++ packages/graphql-playground-react/src/components/Playground/TopBar/ReloadIcon.tsx
@@ -58,8 +58,8 @@ const reloadAction = props => keyframes`
 }`
 
 const Positioner = styled.div`
-  width: 20px;
-  height: 20px;
+  width: 25px;
+  height: 25px;
   cursor: pointer;
   transform: rotateY(180deg);
 `
