98fb9599fad62abd03f473edfbca4f135dbafb8c
ev-dev <evan@quartolio.com>
2018-12-14 07:42:43 -0800

    Added Dockerfile and minimal express server with playground middleware, updated .gitignore to exclude all subdirectory node_modules

---
+++ docker/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressPlayground = require('graphql-playground-middleware-express').default
+
+const defaults = { port: 8080, endpoint: 'http://localhost:3000/graphql' }
+const port = +process.env.PORT || defaults.port
+const endpoint = process.env.ENDPOINT || defaults.endpoint
+
+express()
+	.get('/', expressPlayground({ endpoint }))
+	.listen(port, function(error) {
+		if (error) console.error(error)
+		console.log(
+			`GraphQL Endpoint set to ${endpoint} ${
+				endpoint === defaults.endpoint ? '(Default)' : ''
+			}`,
+			`\n\nServing the GraphQL Playground on http://localhost:${port}/playground ${
+				port === defaults.port ? '(Default)' : ''
+			}`
+		)
+})
