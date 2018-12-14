4a95fc762ea846e1a80b0f8f3ec18402ac2f5540
ev-dev <evan@quartolio.com>
2018-12-14 07:56:26 -0800

    clean yarn.lock

--- docker/server/index.js
+++ docker/server/index.js
@@ -6,16 +6,18 @@ const defaults = { port: 8080, endpoint: 'http://localhost:3000/graphql' }
 const port = +process.env.PORT || defaults.port
 const endpoint = process.env.ENDPOINT || defaults.endpoint
 
-express()
-  .get('/', expressPlayground({ endpoint }))
-  .listen(port, function(error) {
-    if (error) console.error(error)
-    console.log(
-      `GraphQL Endpoint set to ${endpoint} ${
-        endpoint === defaults.endpoint ? '(Default)' : ''
-      }`,
-      `\n\nServing the GraphQL Playground on http://localhost:${port}/playground ${
-        port === defaults.port ? '(Default)' : ''
-      }`
-    )
-  })
+const app = express()
+
+app.get('/', expressPlayground({ endpoint }))
+
+app.listen(port, function(error) {
+  if (error) console.error(error)
+  console.log(
+    `GraphQL Endpoint set to ${endpoint} ${
+      endpoint === defaults.endpoint ? '(Default)' : ''
+    }`,
+    `\n\nServing the GraphQL Playground on http://localhost:${port}/playground ${
+      port === defaults.port ? '(Default)' : ''
+    }`
+  )
+})
