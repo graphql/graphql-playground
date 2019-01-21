fc388f1af183352235212442acd0d1cbd9803aad
Lukáš Huvar <lukas@huvar.cz>
2019-01-21 19:23:32 +0100

    Remove hardcoded version of playground from middlewares

--- packages/graphql-playground-middleware-koa/src/index.ts
+++ packages/graphql-playground-middleware-koa/src/index.ts
@@ -2,25 +2,18 @@ import { Context } from 'koa'
 import {
   MiddlewareOptions,
   renderPlaygroundPage,
-  RenderPageOptions,
 } from 'graphql-playground-html'
 
 /* tslint:disable-next-line */
-const { playgroundVersion } = require('../package.json')
 
 export type KoaPlaygroundMiddleware = (ctx: Context, next: () => void) => void
 
 export type Register = (options: MiddlewareOptions) => KoaPlaygroundMiddleware
 
 const koa: Register = options => {
-  const middlewareOptions: RenderPageOptions = {
-    ...options,
-    version: playgroundVersion,
-  }
-
   return async function voyager(ctx, next) {
     try {
-      ctx.body = await renderPlaygroundPage(middlewareOptions)
+      ctx.body = await renderPlaygroundPage(options)
       await next()
     } catch (err) {
       ctx.body = { message: err.message }
