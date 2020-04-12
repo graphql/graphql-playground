f777ed83ca7ded9c3d5e2184f68453c93a8a34e3
Bram Borggreve <borggreve@gmail.com>
2020-04-12 13:59:24 -0500

    chore(deps): upgrade to GraphQL v15.0.0 (#1194)
    * remove yarn.lock from graphql-playground-electron
    * upgrade to GraphQL v15.0.0
    * update marked because of moderate vulnerability
    * replace introspectionQuery const with getIntrospectionQuery
    
    Co-authored-by: Rikki Schulte <rikki.schulte@gmail.com>

--- packages/graphql-playground-react/src/constants.ts
+++ packages/graphql-playground-react/src/constants.ts
@@ -1,102 +1,11 @@
 import * as cuid from 'cuid'
+import { getIntrospectionQuery } from 'graphql'
 import { getQueryTypes } from './components/Playground/util/getQueryTypes'
 import { List, Map } from 'immutable'
 
 export const columnWidth = 300
 
-export const introspectionQuery = `
-  query IntrospectionQuery {
-    __schema {
-      queryType { name }
-      mutationType { name }
-      subscriptionType { name }
-      types {
-        ...FullType
-      }
-      directives {
-        name
-        description
-        locations
-        args {
-          ...InputValue
-        }
-      }
-    }
-  }
-
-  fragment FullType on __Type {
-    kind
-    name
-    description
-    fields(includeDeprecated: true) {
-      name
-      description
-      args {
-        ...InputValue
-      }
-      type {
-        ...TypeRef
-      }
-      isDeprecated
-      deprecationReason
-    }
-    inputFields {
-      ...InputValue
-    }
-    interfaces {
-      ...TypeRef
-    }
-    enumValues(includeDeprecated: true) {
-      name
-      description
-      isDeprecated
-      deprecationReason
-    }
-    possibleTypes {
-      ...TypeRef
-    }
-  }
-
-  fragment InputValue on __InputValue {
-    name
-    description
-    type { ...TypeRef }
-    defaultValue
-  }
-
-  fragment TypeRef on __Type {
-    kind
-    name
-    ofType {
-      kind
-      name
-      ofType {
-        kind
-        name
-        ofType {
-          kind
-          name
-          ofType {
-            kind
-            name
-            ofType {
-              kind
-              name
-              ofType {
-                kind
-                name
-                ofType {
-                  kind
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+export const introspectionQuery = getIntrospectionQuery()
 
 export const defaultQuery = '# Write your query or mutation here\n'
 
