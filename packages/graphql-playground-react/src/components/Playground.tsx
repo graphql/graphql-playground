4bac122b1204c74e4e63fd1de5cc3bd611ccdabb
Yoshi Sukeda <yoshiaki_ssss@yahoo.com>
2020-04-15 11:46:37 +0900

    Fix/not update schema (#1030)
    * fix(graphql-playground-react): not update schema
    
    Schema isn't updated from the previous one after getting a new graphql schema by introspection query unless headers are set. This commit fixes it.
    
    * docs(graphql-playground-react): correct types of props
    
    Co-authored-by: Rikki Schulte <rikki.schulte@gmail.com>

--- packages/graphql-playground-react/src/components/Playground.tsx
+++ packages/graphql-playground-react/src/components/Playground.tsx
@@ -84,7 +84,7 @@ export interface Props {
   configIsYaml: boolean
   canSaveConfig: boolean
   fixedEndpoints: boolean
-  headers?: any
+  headers?: { [key: string]: string }
   configPath?: string
   createApolloLink?: (
     session: Session,
@@ -104,7 +104,10 @@ export interface ReduxProps {
   saveConfig: () => void
   saveSettings: () => void
   setTracingSupported: (value: boolean) => void
-  injectHeaders: (headers: string, endpoint: string) => void
+  injectHeaders: (
+    headers: string | { [key: string]: string } | void,
+    endpoint: string,
+  ) => void
   setConfigString: (str: string) => void
   schemaFetchingError: (endpoint: string, error: string) => void
   schemaFetchingSuccess: (
@@ -258,7 +261,9 @@ export class Playground extends React.PureComponent<Props & ReduxProps, State> {
         headers:
           props.sessionHeaders && props.sessionHeaders.length > 0
             ? props.sessionHeaders
-            : JSON.stringify(props.headers),
+            : props.headers && Object.keys(props.headers).length > 0
+              ? JSON.stringify(props.headers)
+              : undefined,
         credentials: props.settings['request.credentials'],
         useTracingHeader:
           !this.initialSchemaFetch &&
