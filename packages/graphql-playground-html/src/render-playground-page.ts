b7e6d4d7590766183a77910a517ea946b95f2a84
Eden Tyler-Moss <edenmossie@gmail.com>
2020-09-15 15:48:13 +0100

    fix: add schema.polling* to ISettings interface. (#1212)

--- packages/graphql-playground-html/src/render-playground-page.ts
+++ packages/graphql-playground-html/src/render-playground-page.ts
@@ -27,6 +27,9 @@ export interface ISettings {
   'editor.fontSize': number
   'editor.fontFamily': string
   'request.credentials': string
+  'schema.polling.enable': boolean
+  'schema.polling.endpointFilter': string
+  'schema.polling.interval': number
 }
 
 export interface EditorColours {
